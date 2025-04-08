5c307a8e0bb61614108d1509237f652f40cf04c3
zinebchennit <zinebhadyl@gmail.com>
2025-04-08 04:20:09 +0200

    Ajout du dossier

---
+++ src/MonComposant.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MonComposant() {
+  return (
+    <div>
+      <h2>Bienvenue dans mon composant</h2>
+      <p>Ce contenu vient d'un composant React !</p>
+    </div>
+  );
+}
+
+export default MonComposant;
